21c653a1b436e2de735e9a764ef8dc9893dc5323
Mr. Macfadden <37188696+mrmacfadden@users.noreply.github.com>
2025-08-26 15:02:23 -0500

    genesis

---
+++ swatch/genesis.php
@@ -0,0 +1,135 @@
+<?php
+    include '../header.php'; // Include header
+?>
+<div class="swatch-hero py-5" style="background-color:#172844;">
+    <div class="container my-4 shadow-img">
+
+        <div class="row row-cols-5 mt-4">
+            <div class="col d-flex justify-content-center align-items-center mb-4">
+                <img src="img/000001.png" class="img-fluid" alt="Genesis">
+            </div>
+            <div class="col d-flex justify-content-center align-items-center mb-4">
+                <img src="img/000002.png" class="img-fluid" alt="Genesis">
+            </div>
+            <div class="col d-flex justify-content-center align-items-center mb-4">
+                <img src="img/000003.png" class="img-fluid" alt="Genesis">
+            </div>
+            <div class="col d-flex justify-content-center align-items-center mb-4">
+                <img src="img/000004.png" class="img-fluid" alt="Genesis">
+            </div>
+            <div class="col d-flex justify-content-center align-items-center mb-4">
+                <img src="img/000005.png" class="img-fluid" alt="Genesis">
+            </div>
+        </div>
+
+        <div class="row row-cols-5 mt-4">
+            <div class="col d-flex justify-content-center align-items-center mb-4">
+                <img src="img/000006.png" class="img-fluid" alt="Genesis">
+            </div>
+            <div class="col d-flex justify-content-center align-items-center mb-4">
+                <img src="img/000007.png" class="img-fluid" alt="Genesis">
+            </div>
+            <div class="col d-flex justify-content-center align-items-center mb-4">
+                <img src="img/000008.png" class="img-fluid" alt="Genesis">
+            </div>
+            <div class="col d-flex justify-content-center align-items-center mb-4">
+                <img src="img/000009.png" class="img-fluid" alt="Genesis">
+            </div>
+            <div class="col d-flex justify-content-center align-items-center mb-4">
+                <img src="img/000010.png" class="img-fluid" alt="Genesis">
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<div class="container my-4 shadow-img">
+    <div class="row mt-4 pb-5" id="swatch-description">
+        <h1>Genesis Series</h1>
+        <h4>By Michael Macfadden</h4>
+        <h3>What’s a Crypto Swatch?</h3>
+        <p>
+        The Crypto Swatch project launched on February 28th, 2021 on the
+        <strong><a href="https://on.wax.io/wax-io/" target="_blank" rel="noopener noreferrer">WAX Blockchain</a></strong>.
+        The original collection included 10 different colors: Brown, Purple, Red, Orange, Yellow, Green, Blue, Black, Pink, and Grey. 
+        Only ten of each color, for a total of 100 non-fungible tokens (NFTs), were originally minted.
+        </p>
+        <p>
+        While anyone could copy and paste the source images, they would be in possession of a virtual forgery 
+        (akin to holding a print of the Mona Lisa as opposed to the Mona Lisa itself). 
+        Only the token holder can claim true ownership.
+        </p>
+
+        <h3>How to Get Your Own Crypto Swatch</h3>
+        <p>
+        If you were fortunate enough to get one of the original 100, congratulations! None of those will ever be minted again.
+        </p>
+        <p>
+        If you missed out on the initial drop, but would still like to be a part of history, sign up for a 
+        <a title="Wax Wallet" href="https://all-access.wax.io/" target="_blank" rel="noopener noreferrer"><strong>Wax Wallet</strong></a>
+        to obtain a public key (sort of like an email address for digital collectibles), send it to me via Twitter 
+        <a href="https://twitter.com/mjmacfadden"><strong>@mjmacfadden</strong></a> 
+        (or leave it in the comments section below), and I’ll issue you a Deep Teal Crypto Swatch (learn more about the Deep Teal Crypto Swatch below).
+        </p>
+        <p>
+        Once received, you can trade it with another wax wallet holder, request another, post it to social media, or even try to sell it on a secondary market like 
+        <a href="https://wax.atomichub.io/" target="_blank" rel="noopener noreferrer"><strong>Atomic Hub</strong></a>.
+        </p>
+    </div>
+</div>
+<div class="grey-bg py-5">
+
+    <div class="container my-4 shadow-img swatch-description">
+        <div class="row mt-4 align-items-start">
+                <!-- Image column -->
+                <div class="col-12 col-sm-3 mb-3 mb-md-0 d-flex justify-content-center">
+                    <img src="img/000011.png" style="width:100%; max-width:414px;" alt="Crypto Swatch" title="000011" class="img-fluid">
+                </div>
+
+                <!-- Text column -->
+                <div class="col-12 col-sm-9">
+                    <h3 class="text-start">Turn It Up to 11</h3>
+                    <p>
+                        The initial batch of Crypto Swatches were received with a startling level of acceptance and enthusiasm from the NFT Community. 
+                        Demand far exceeded supply! Thus, I’ve decided to turn things up to 11, bring a new crypto swatch to market, and remove the mint limit.
+                    </p>
+                    <p>
+                        The Deep Teal Crypto Swatch is intended to be the every-man-and-woman’s NFT—an entry point for anyone interested in Digital Collectibles. 
+                        Unlike the first 100, the Deep Teal Crypto Swatch is abundant. Request one, trade it, ask for another, and give that one to a friend.
+                    </p>
+                    <p>Help spread the Crypto Swatch project!</p>
+                </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="container swatch-description my-4 shadow-img">
+    <h3>Why Crypto Swatches</h3>
+    <p>
+    NFTs have set their hooks in me. I find the space fascinating, and I’m not even entirely sure why, 
+    but I learn best by doing, so here I am exploring every nook and cranny of the space that I can.
+    </p>
+
+    <h3>What’s Next</h3>
+    <p>
+    The primary objective of this project is to maximize my understanding of the NFT space, 
+    but if Crypto Swatches happen to resonate with enough other people, 
+    I do have napkin-sketch-plans for a dApp that would gamify the collection experience 
+    by introducing build and burn mechanics that would reveal new, even rarer colors, patterns, and gradients!
+    </p>
+
+    <div class="row pt-5">
+        <div class="col-lg-6">
+            <a href="index.php"><i class="bi bi-arrow-left"></i> HOME</a>
+        </div>
+        <div class="col-lg-6 text-end">
+            <a href="swatch/spring-collection.php">SPRING COLLECTION <i class="bi bi-arrow-right"></i></a>
+        </div>
+    </div>
+</div>
+
+
+<?php
+    include '../footer.php'; // Include footer
+?>
