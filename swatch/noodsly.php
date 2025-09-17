6e25e6ee7f3cd4a67f56eaa41ea31a8cc2efabdc
Mr. Macfadden <37188696+mrmacfadden@users.noreply.github.com>
2025-09-17 15:24:54 -0500

    foo

---
+++ swatch/noodsly.php
@@ -0,0 +1,79 @@
+<?php
+     $og_image = 'https://mjmacfadden.github.io/cryptoswatches/img/featured-img/noodsly-featured-img.jpg'; // Set before including header
+    include '../header.php'; // Include header
+?>
+<div class="swatch-hero py-5" style="background-color:#FB9669;">
+    <div class="container my-4 shadow-img">
+        <div class="d-flex justify-content-center align-items-center mb-4">
+            <img src="img/000314.png" class="img-fluid" alt="">
+        </div>
+        <a href="swatch/noodsly.php#swatch-description">
+            <i class="bi bi-chevron-double-down"></i>
+        </a>
+    </div>
+</div>
+
+<div class="container my-4 shadow-img">
+    <div class="row mt-4 pb-5" id="swatch-description">
+        <h1>Noodsly</h1>
+        <h4>by Michael Macfadden</h4>
+        <p><strong>Please introduce yourself to our readers:</strong></p>
+        <p>So in the real world, i am a content production, as well as a general digital artist ( illustration to mographs to 3D production ). 🙂 I have worked on producing tv series to commercials. Along that line, I managed to learn more into character branding. And that&rsquo;s where I am super interested in doing now. After much research and development that&rsquo;s how Noodsly came about 😀</p>
+        <p><strong>What was the inspiration for this piece?</strong></p>
+        <p>Noodsly is a noodle character. What they are trying to do is simply to create a community and have fun conversation around food. Food with interesting history around it, food that is personal and passed down through generations or maybe it is about the recipe and ingredients that makes the food interesting. Noodsly is still at its early stage at the moment with just me manning the brand, but what slowly Noodsly would be is a character that spread stories and interesting facts about food from the community it builds. Not only will it thrive in NFT realm but Noodsly would extends stories around food through other social media like Instagram and Twitter.</p>
+        <p><strong>How long have you been creating in the digital realm?</strong></p>
+        <p>Noodsly has been active in NFT around end of September in 2021.</p>
+        <p><strong>Can you tell us about your workflow (what equipment and software do you use to create)?</strong></p>
+        <p>I mostly use procreate to illustrate but if there&rsquo;s any animation involved, then I would use After Effects.</p>
+        <p><strong>Who are your artistic influences?</strong></p>
+        <p>Gaston Pacheco, Eiichiro Oda (One Piece creator), Kaws</p>
+        <p><strong>What got you interested in NFTs/Crypto Art/Digital Collectibles?</strong></p>
+        <p>I think sooner or later everyone would have to delve into NFT. There is definitely a long future ahead into crypto. For me I&rsquo;m not in it for the fast turn around, I am in it to build a wider community to have talk about FOOD! haha. And I want to build Noodsly steadily to however much i can cope but consistent. In the end I just want people who collect my NFT to like it and to want to keep them because they just love Noodsly and what they stand for.</p>
+        <p><strong>Where can we find you online?</strong></p>
+        <ul>
+        <li>NeftyBlocks:&nbsp;<a dir="ltr" title="https://neftyblocks.com/c/noodslynoods/" href="https://t.co/YWTIoGuYAP" target="_blank" rel="noopener noreferrer">https://neftyblocks.com/c/noodslynoods/</a></li>
+        <li>Instagram:&nbsp;<a title="@noodsly" href="https://instagram.com/noodsly" target="_blank" rel="noopener">@noodsly</a></li>
+        <li>Twitter:&nbsp;<a dir="ltr" title="@noodsly" href="https://twitter.com/noodsly" target="_blank" rel="noopener">@noodsly</a></li>
+        <li>Discord:&nbsp;<a dir="ltr" href="https://t.co/KH9yKqae9Y" target="_blank" rel="noopener noreferrer">https://discord.gg/vwAdxRCfC4</a></li>
+        </ul>
+        <p>(I am collaborating with the Nifty Art Collective team in minting Noodsly&rsquo;s NFT out, Noodsly has their own channel in there that is accesable by purchasing any of Noodsly&rsquo;s NFT.)</p>
+        <p><strong>Anything else you&rsquo;d like to share?</strong></p>
+        <p>Thank you peeps who is interesting in getting Noodsly first food blending! There will be more of this to come in Noodslynoods collection and if you like what you see here, please continue watch over or even better chitty chatty chat chat with me over in Twitter or Discord 🙂 Cheers!</p>
+    </div>
+</div>
+
+<div class="swatch-footer py-5" style="background-color:#FDCAB4;">
+    <div class="container my-4 shadow-img">
+        <div class="row mt-4">
+            <div class="col d-flex justify-content-center align-items-center mb-4">
+                <img src="img/000313.png" class="img-fluid lightbox" alt="Noodsly">
+            </div>
+            <div class="col d-flex justify-content-center align-items-center mb-4">
+                <img src="img/000314.png" class="img-fluid lightbox" alt="Noodsly">
+            </div>
+            <div class="col d-flex justify-content-center align-items-center mb-4">
+                <img src="img/000315.png" class="img-fluid lightbox" alt="Noodsly">
+            </div>
+            <div class="col d-flex justify-content-center align-items-center mb-4">
+                <img src="img/000316.png" class="img-fluid lightbox" alt="Noodsly">
+            </div>
+            <div class="col d-flex justify-content-center align-items-center mb-4">
+                <img src="img/000317.png" class="img-fluid lightbox" alt="Noodsly">
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <a href="swatch/space-chicken-3.php"><i class="bi bi-arrow-left"></i> SPACE CHICKEN 3</a>
+            </div>
+            <div class="col-6 text-end">
+                <a href="swatch/senor-lupe.php">SEÑOR LUPE <i class="bi bi-arrow-right"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php
+    include '../footer.php'; // Include footer
+?>
