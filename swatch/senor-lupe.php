26cffb1b91d62619de7d99a982d6e5a4ca7cf353
Mr. Macfadden <37188696+mrmacfadden@users.noreply.github.com>
2025-09-18 09:15:25 -0500

    foo

---
+++ swatch/senor-lupe.php
@@ -0,0 +1,71 @@
+<?php
+     $og_image = 'https://mjmacfadden.github.io/cryptoswatches/img/featured-img/senor-lupe-featured-img.jpg'; // Set before including header
+    include '../header.php'; // Include header
+?>
+<div class="swatch-hero py-5" style="background-color:#54FDF6;">
+    <div class="container my-4 shadow-img">
+        <div class="d-flex justify-content-center align-items-center mb-4">
+            <img src="img/000320.png" class="img-fluid" alt="">
+        </div>
+        <a href="swatch/senor-lupe.php#swatch-description">
+            <i class="bi bi-chevron-double-down"></i>
+        </a>
+    </div>
+</div>
+
+<div class="container my-4 shadow-img">
+    <div class="row mt-4 pb-5" id="swatch-description">
+        <h1>Señor Lupe</h1>
+        <h4>by Michael Macfadden</h4>
+        <p><strong>Please introduce yourself to our readers:</strong>hola amigos! i is lupe, cryptos traders y nft creators! is been in cryptos twitters space since 2018! i is lead the peoples thru the bear markets. is tell them to no be scare. we make is to 2021 an nfts is blow up! so lupe is test my skills and start is my collections on wax blockchains! is start with pixels art an has now incorporate is ipads arts! i is texas mans, live is in rancho de lupe, is luv horse an is own the bes horse in the universes, mr chummy! is my favor horse, so fas, so strong, is never has fear, no ever. is my motivations to start is 1-1 pfp collections on wax call chummys! i drop is 100 at a times! series 3 is drop febuary 2022!</p>
+        <p><strong>What was the inspiration for this piece?</strong></p>
+        <p>i is rly wanted to incorporate is swatch into my piece. i is do research to look is at many swatches! so many beautiful colors! is decides to make chummy swatch collabs! is firs ever chummys thas a fabrics! borders is make like a swatch, an is add my favor sunsets gradients for u eyes!</p>
+        <p><strong>How long have you been creating in the digital realm?</strong></p>
+        <p>i has been creating nfts in wax blockchains since is jan 2021. is been whole yr now! is been so fun, an is been a blessings to be able to art is agains! i is always luv to art, but nft is a revolutions. artis can find they collectors easys now! is rly change lupe life.</p>
+        <p><strong>Can you tell us about your workflow (what equipment and software do you use to create)?</strong></p>
+        <p>wen lupe is do pixel arts, i is use pixelart.com or aseprite. jus depens on piece. if is non pixel, i is use my ipads! procreates y adobe fresco my two apps i is luv the mos right now. wen i art is a piece, i is has to be excite about is or i will no release is. the art is has to make lupe excite to do is, then i kno my frens is be excite about it to. i no rush, no ever. i take is my time an wen i is feel universe has deliver the art, i put is into blockchains eternities!</p>
+        <p><strong>Who are your artistic influences?</strong></p>
+        <p>is has to says my 80s/90s youth is influence lupe the mos. nostalgias is my favor. is luv all things from childhoods, so i bring is to my arts. is also also drawn to cartoonis style arts, an also street arts. i can graffiti is a bit, has no debut is yet, but i is do other art styles! i no ever is wanna limits lupe to jus one. i experimens with all an hope mi amigos is luv them all!</p>
+        <p><strong>What got you interested in NFTs/Crypto Art/Digital Collectibles?</strong></p>
+        <p>is early 2021 an is has amigos tell lupe wen merch? wen u drop items for u frens? an i never has drop anything evers. but is see nfts blow up, is has mi amigo senor crypto euclids who is push lupe to drop somethings! an mi amigo senor d0c0s also is push is lupe to drop nfts for wax. i also then is see senor kenn bosaks an all he is do. senor stache another amigo thas lead is for lupe. so i is start an is go the entire yrs makin arts for mi amigos!</p>
+        <p><strong>Where can we find you online?</strong></p>
+        <p>bes place to find u fren lupe is twitters. dm is open always for u. also u can find is in my telegrams channels! NO SCARE. NO EVER. my arts is live in wax blockchains so i do is all drops on nefty blocks. then my secondarys nfts all list on atomic hubs.</p>
+        <p><strong>Anything else you&rsquo;d like to share?</strong></p>
+        <p>i is wanna says gracias to amigo swatches for oppurtunity to collabs! is was a real fun one to do! is wanna says gracias to all wax familia for u luv an support. is my honors to be u fren. series 3 chummys be is rdy for feb 2022 drop. an we ride is amigo. always is ride is no matter price of coins. we gonna build is the bes cryptos nft communitys thas ever exist! jus has to has diamons wen is down, an focuses wen is up! SALUD SENOR SWATCHES! SALUD WAX FAMILIAS!! VIVA LA WAX!!</p>
+    </div>
+</div>
+
+<div class="swatch-footer py-5" style="background-color:#A9FEFA;">
+    <div class="container my-4 shadow-img">
+        <div class="row mt-4">
+            <div class="col d-flex justify-content-center align-items-center mb-4">
+                <img src="img/000318.png" class="img-fluid lightbox" alt="">
+            </div>
+            <div class="col d-flex justify-content-center align-items-center mb-4">
+                <img src="img/000319.png" class="img-fluid lightbox" alt="">
+            </div>
+            <div class="col d-flex justify-content-center align-items-center mb-4">
+                <img src="img/000320.png" class="img-fluid lightbox" alt="">
+            </div>
+            <div class="col d-flex justify-content-center align-items-center mb-4">
+                <img src="img/000321.gif" class="img-fluid lightbox" alt="">
+            </div>
+            <div class="col d-flex justify-content-center align-items-center mb-4">
+                <img src="img/000322.gif" class="img-fluid lightbox" alt="">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <a href="swatch/noodsly.php"><i class="bi bi-arrow-left"></i> NOODSLY</a>
+            </div>
+            <div class="col-6 text-end">
+                <a href="swatch/corpworldnft.php">CORPWORLD_NFT <i class="bi bi-arrow-right"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php
+    include '../footer.php'; // Include footer
+?>
