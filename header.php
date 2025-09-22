825890c0dbe5c6179e919789440a42e45df6e45d
Mr. Macfadden <37188696+mrmacfadden@users.noreply.github.com>
2025-09-22 08:44:46 -0500

    base attribute

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
     ?>
     <meta property="og:image" content="<?php echo $og_image_path; ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <base href="/cryptoswatches/" />
+    <base href="/" />
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
